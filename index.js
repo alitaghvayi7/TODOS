59f9761ef5ffe5225e987602a8941b315f61ae7c
alitaghvayi7 <alitaghvaei78@gmail.com>
2023-02-16 18:21:50 +0330

    add edit and delete

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
 const toggleButton = document.querySelector('.toggle-button');
 const cancelButton = document.querySelector('.cancel-button');
-const saveButton = document.querySelector('.save-button');
+const form = document.querySelector('.form');
 const tasksContainer = document.querySelector('.tasks-container');
 const searchInput = document.querySelector('.search-input');
 
 let tasks;
 
-
+// form Animation
 const timeLine = gsap.timeline({
     paused: true,
     defaults: {
@@ -23,14 +22,21 @@ timeLine
     .from('.save-button', { opacity: 0, x: -100 });
 
 
+
+// Element Listeners
 window.addEventListener('load', init)
 toggleButton.addEventListener('click', showOverlay);
 cancelButton.addEventListener('click', hiddenOverlay);
-saveButton.addEventListener('click', addTask);
+form.addEventListener('submit', addTask);
 searchInput.addEventListener('input', searchBetweenTasks);
 
+function init() {
+    tasks = getTasksFromLocalStorage() || [];
+    renderTasks(tasks);
+}
 
-function addTask() {
+function addTask(event) {
+    event.preventDefault();
     const todoInput = document.querySelector('.todo-input');
 
     if (!todoInput.value) return;
@@ -43,39 +49,32 @@ function addTask() {
     setTasksToLocalStorage(tasks);
 }
 
-function searchBetweenTasks(event) {
+function editTask(event) {
 
-    const value = event.currentTarget.value;
+    event.stopPropagation();
 
-    const results = tasks.filter((task) => {
-        return task.value.includes(value);
-    });
+    const { currentTarget } = event;
 
-    if (results.length === 0) {
-        tasksContainer.textContent = "موردی برای نمایش وجد ندارد";
-        return
-    }
+    const taskElement = event.currentTarget.closest('.task-item');
+    const index = taskElement.getAttribute('data-index');
 
-    renderTasks(results);
+    currentTarget.removeEventListener("click", editTask);
+    currentTarget.addEventListener('click', saveChanges.bind(null, index));
+
+    currentTarget.textContent = "save";
+
+    //hidden the span tag that show task value
+    currentTarget.parentElement.previousElementSibling.style.display = "none";
+    const { value } = tasks[index];
+    taskElement.prepend(createInputBox(value));
+
 }
 
-function renderTasks(array) {
-    tasksContainer.textContent = ""
-    const elements = array.map((task) => {
-        return createTaskItem(task);
-    });
-
-    elements.forEach((elem) => {
-        tasksContainer.append(elem)
-    });
-}
-
-function createTaskItem(item) {
-    const div = document.createElement('div');
-    div.className = item.isFinished === true ? 'task-item completed' : 'task-item';
-    div.textContent = item.value;
-    div.addEventListener('click', finishTask);
-    return div;
+function deleteTask(event) {
+    event.stopPropagation();
+    const index = event.currentTarget.closest('.task-item').getAttribute('data-index');
+    tasks.splice(index, 1);
+    renderTasksAndSaveToLocalStorage(tasks)
 }
 
 function finishTask(event) {
@@ -86,17 +85,106 @@ function finishTask(event) {
 
     if (tasks[taskIndex].isFinished) return;
 
-    console.log('return');
-
     tasks[taskIndex].isFinished = true;
 
+    renderTasksAndSaveToLocalStorage(tasks)
+}
+
+function searchBetweenTasks(event) {
+
+    const value = event.currentTarget.value;
+
+    const results = tasks.filter((task) => {
+        return task.value.includes(value);
+    });
+
+    if (results.length === 0) {
+        tasksContainer.textContent = "موردی برای نمایش وجود ندارد";
+        return
+    }
+
+    renderTasks(results);
+}
+
+function createTaskItem(item, index) {
+    const div = document.createElement('div');
+    div.className = item.isFinished === true ? 'task-item completed' : 'task-item';
+    div.setAttribute('data-index', index);
+
+    const textTask = document.createElement('span');
+    textTask.classList = 'text-task';
+    textTask.textContent = item.value;
+    textTask.addEventListener('click', finishTask);
+    div.appendChild(textTask)
+
+
+    const taskOptions = document.createElement('div');
+    taskOptions.className = "task-options";
+
+    const editButton = document.createElement('button');
+    editButton.className = "edit-task";
+    editButton.addEventListener('click', editTask);
+    editButton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>';
+    taskOptions.appendChild(editButton);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.className = "delete-task";
+    deleteButton.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
+    deleteButton.addEventListener('click', deleteTask);
+    taskOptions.appendChild(deleteButton);
+
+    div.appendChild(taskOptions)
+
+    return div;
+}
+
+function renderTasksAndSaveToLocalStorage(tasks) {
     renderTasks(tasks);
     setTasksToLocalStorage(tasks);
 }
 
-function init() {
-    tasks = getTasksFromLocalStorage() || [];
-    renderTasks(tasks);
+function renderTasks(array) {
+    tasksContainer.textContent = ""
+    const elements = array.map((task, index) => {
+        return createTaskItem(task, index);
+    });
+
+    elements.forEach((elem) => {
+        tasksContainer.append(elem)
+    });
+}
+
+function saveChanges(index, event) {
+
+    const { currentTarget } = event;
+
+    currentTarget.removeEventListener('click', saveChanges);
+    currentTarget.addEventListener('click', editTask);
+    currentTarget.textContent = "e";
+
+    //get the input
+    const input = event.currentTarget.parentElement.previousElementSibling.previousElementSibling;
+    //get the span
+    const span = event.currentTarget.parentElement.previousElementSibling;
+
+    const currentObject = tasks[index];
+    const newObject = { value: input.value, isFinished: currentObject.isFinished };
+
+    input.remove();
+    span.style.display = "block";
+
+    tasks.splice(index, 1, newObject);
+    renderTasksAndSaveToLocalStorage(tasks);
+
+}
+
+function createInputBox(value) {
+    const input = document.createElement('input');
+    input.className = "task-edit-input";
+    input.type = "text";
+    input.setAttribute('value', value);
+    input.style.display = "block";
+    return input;
 }
 
 function showOverlay(e) {
@@ -114,7 +202,6 @@ function getTasksFromLocalStorage() {
     tasks = JSON.parse(tasks);
     return tasks;
 }
-
 
 function setTasksToLocalStorage(item) {
     const value = JSON.stringify(item);
